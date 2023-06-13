38f79c6cf1314bd25ab88c4b88fbd7c2b1ef9212
Samir Amin <aminsamir45@gmail.com>
2023-06-12 21:09:34 -0700

    initialization

---
+++ visualizations/sheet_func.py
@@ -0,0 +1,27 @@
+# Extract the document ID from the URL - middle id
+import pandas as pd
+import requests
+
+document_id = "1c6HTfHQfp7MIg1LFo6_wFOzKLIHA1rzVpDxXIPosRQ0"
+
+def sheet_to_df(sheet_id: int, document_id = document_id) -> pd.DataFrame:
+    
+    """ Function that takes in a google sheet's id number and document id as input and returns
+        the dataframe of the particular sheet as output """
+    
+    # CSV download link
+    csv_url = f"https://docs.google.com/spreadsheets/d/{document_id}/export?format=csv&gid={sheet_id}"
+
+    # Download the CSV file
+    response = requests.get(csv_url)
+    csv_data = response.content
+
+    # Save the CSV data to a file
+    csv_file = "ROI simplified visualizations.csv"
+    with open(csv_file, "wb") as file:
+        file.write(csv_data)
+
+    # Read the CSV file into a DataFrame using pandas
+    df = pd.read_csv(csv_file)
+    
+    return df
